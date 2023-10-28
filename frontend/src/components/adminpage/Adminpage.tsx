f92a3df9a4a7e9a8d2e210c262b5657985f91538
florian <florian@sandel.email>
2023-10-28 17:51:05 +0200

    split admin route and admin page

--- frontend/src/components/adminpage/Adminpage.tsx
+++ frontend/src/components/adminpage/Adminpage.tsx
@@ -1,7 +1,57 @@
+import React, { useState } from "react";
+
 const Adminpage: React.FC = () => {
+  const [eventId, setEventId] = useState("");
+
+  const handlePostEvent = () => {
+    fetch("http://localhost:4000/event", {
+      method: "POST",
+      mode: "no-cors", // Set the mode to "no-cors"
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ eventId }),
+    })
+      .then((response) => {
+        console.log("POST request successful");
+      })
+      .catch((error) => {
+        console.error("Error making POST request:", error);
+      });
+  };
+
+  const handlePostSetup = () => {
+    fetch("http://localhost:4000/setup", {
+      method: "POST",
+      mode: "no-cors", // Set the mode to "no-cors"
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ eventId }),
+    })
+      .then((response) => {
+        console.log("POST request successful");
+      })
+      .catch((error) => {
+        console.error("Error making POST request:", error);
+      });
+  };
+
   return (
     <>
-      <h1>Admin Page</h1>
+      <div>
+        <h1>Admin Page</h1>
+        <input
+          type="text"
+          placeholder="Enter Event ID"
+          value={eventId}
+          onChange={(e) => setEventId(e.target.value)}
+        />
+        <button onClick={handlePostEvent}>Post Request</button>
+        <p></p>
+        <p></p>
+        <button onClick={handlePostSetup}>Refresh Data</button>
+      </div>
     </>
   );
 };
--- frontend/src/routes/Admin.tsx
+++ frontend/src/routes/Admin.tsx
@@ -1,59 +1,11 @@
-import React, { useState } from "react";
+import Adminpage from "../components/adminpage";
 
-const Adminpage: React.FC = () => {
-  const [eventId, setEventId] = useState("");
-
-  const handlePostEvent = () => {
-    fetch("http://localhost:4000/event", {
-      method: "POST",
-      mode: "no-cors", // Set the mode to "no-cors"
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ eventId }),
-    })
-      .then((response) => {
-        console.log("POST request successful");
-      })
-      .catch((error) => {
-        console.error("Error making POST request:", error);
-      });
-  };
-
-  const handlePostSetup = () => {
-    fetch("http://localhost:4000/setup", {
-      method: "POST",
-      mode: "no-cors", // Set the mode to "no-cors"
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ eventId }),
-    })
-      .then((response) => {
-        console.log("POST request successful");
-      })
-      .catch((error) => {
-        console.error("Error making POST request:", error);
-      });
-  };
-
+const Admin: React.FC = () => {
   return (
     <>
-      <div>
-        <h1>Admin Page</h1>
-        <input
-          type="text"
-          placeholder="Enter Event ID"
-          value={eventId}
-          onChange={(e) => setEventId(e.target.value)}
-        />
-        <button onClick={handlePostEvent}>Post Request</button>
-        <p></p>
-        <p></p>
-        <button onClick={handlePostSetup}>Refresh Data</button>
-      </div>
+      <Adminpage />
     </>
   );
 };
 
-export default Adminpage;
+export default Admin;
